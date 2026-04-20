f7537ba883cbbb9d40ae55342c44baa9260558d6
Tenguu95 <tonypabst95@web.de>
2026-04-20 11:25:10 +0200

    port fix

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ def login():
 def logout():
     session.clear() 
     return redirect("/login")
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
